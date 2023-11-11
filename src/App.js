4df88975d7a27d4efc6f11ad7849361bd7302fbe
Dmitriy <shulginmma@gmail.com>
2023-11-11 22:40:12 +0300

    App.js by js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 export const App = () => {
-	// Императивный стиль : получение текущего года
 	const currentYear = new Date().getFullYear();
-	// Декларативный стиль: описание структуры интерфейса с использованием JSX.
-	return (
-		<div className="App">
-			<header className="App-header">
-				{/* Декларативный стиль: использование изображения для логотипа. */}
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					{/* Декларативный стиль: описание текста. */}
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				{/* Декларативный стиль: создание ссылки. */}
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>
-					{/* Декларативный стиль: вывод текущего года. */}
-					<code>{currentYear}</code>
-				</p>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p'),
+			createElement('code', null, currentYear),
+		),
 	);
 };
